f62952d59d1ee754e7c572aa86ec99534202ba4a
Mikkel <mikkel.struntze@me.com>
2013-01-30 09:44:19 +0100

    Fixed to use the new NetworkVariables

--- With Owner Text/weed_plant/cl_init.lua
+++ With Owner Text/weed_plant/cl_init.lua
@@ -1,11 +1,11 @@
 include("shared.lua")
 
 function ENT:Draw()
-
-local Pos = self:GetPos()
+	self:DrawModel()
+	local Pos = self:GetPos()
 	local Ang = self:GetAngles()
 
-	local owner = self.dt.owning_ent
+	local owner = self:Getowning_ent()
 	owner = (IsValid(owner) and owner:Nick()) or "Unknown"
 
 	surface.SetFont("HUDNumber5")
@@ -14,19 +14,14 @@ local Pos = self:GetPos()
 
 	Ang:RotateAroundAxis(Ang:Forward(), 90)
 	local TextAng = Ang
-
-	
-
 	cam.Start3D2D(Pos + Ang:Right() * -30, TextAng, 0.2)
 		draw.WordBox(1, -TextWidth*0.5 + 5, -60, "Weed Plant", "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
 		draw.WordBox(1, -TextWidth2*0.5 + 5, -14, owner, "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
 	cam.End3D2D()
 	
-if self.Entity:GetNWBool("Usable") == true then
+	if self.Entity:GetNWBool("Usable") == true then
 		self.Entity:SetColor( Color( 0, 255, 0, 255) )
 	else
 		self.Entity:SetColor( Color( 255, 255, 255, 255) )
 	end
-	self.Entity:DrawModel()
-
 end
